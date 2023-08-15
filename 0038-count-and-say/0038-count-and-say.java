217a38441f18a9904053f8f961c0ea0ed39346d9
Ankit Pathak <78856709+ankitpathak530@users.noreply.github.com>
2023-08-15 20:35:18 +0530

    Time: 1 ms (99.45%), Space: 39.6 MB (98.53%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,46 +1,28 @@
 class Solution {
-    //5450
     public String countAndSay(int n) {
-        
-         if(n == 1){
+        if (n == 1) {
             return "1";
         }
-        
-        
-        String res = countAndSay(n-1); //11
-        
-        StringBuffer  finalRes = new StringBuffer("");
-        
-        char pch = res.charAt(0); // get first character
-        int count = 1;
-        
-        
-            
-        for(int i=1; i<res.length(); i++){
-              
-                char ch = res.charAt(i); //to get ith character
 
-                if(pch == ch)
-                    count++;
-                else{
-                    finalRes.append(count).append(pch);
-                    count = 1; //reset the count
-                    pch = ch;                
-                 }
-         }
-        
+        String res = countAndSay(n - 1);
+
+        StringBuilder finalRes = new StringBuilder();
+        char pch = res.charAt(0);
+        int count = 1;
+
+        for (int i = 1; i < res.length(); i++) {
+            char ch = res.charAt(i);
+
+            if (pch == ch) {
+                count++;
+            } else {
+                finalRes.append(count).append(pch);
+                count = 1;
+                pch = ch;
+            }
+        }
+
         finalRes.append(count).append(pch);
-      
-         
-        
-        System.out.println("Res->"+res+ "    FinalRes->  "+finalRes);
-        
-        
-        
         return finalRes.toString();
-        
     }
-    
-  
-  
 }
