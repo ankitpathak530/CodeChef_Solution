e590c20a9970966fff5d8d19e36a03892f6a095b
Ankit Pathak <78856709+ankitpathak530@users.noreply.github.com>
2023-09-11 14:39:27 +0530

    Time: 7 ms (55.15%), Space: 44.5 MB (59.12%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,41 +1,18 @@
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.concurrent.atomic.AtomicInteger;
-
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-          
-        List<Integer>[] res = new ArrayList[500];
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<500; i++) res[i] = new ArrayList<>();
+        List<List<Integer>> allGroups = new ArrayList<>();
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
 
-        List<List<Integer>> finalRes = new ArrayList<>();
-
-
-        for(int i=0; i<groupSizes.length; i++){
-            int idx = groupSizes[i]; //3
-            set.add(idx);
-            res[idx].add(i);
+        for (int i = 0; i < groupSizes.length; i++){
+            ArrayList<Integer> list = map.getOrDefault(groupSizes[i], new ArrayList<>());
+            map.put(groupSizes[i], list);
+            list.add(i);
+    
+            if (list.size() == groupSizes[i]){
+                allGroups.add(new ArrayList<>(list));
+                map.put(groupSizes[i], new ArrayList<>());
+            }
         }
-
-
-        for(int i : set){ 
-            List<Integer> element = res[i];
-            // i -> 3    elemstSize- 6
-            AtomicInteger counter = new AtomicInteger();
-            System.out.println("Counter: - "+counter);
-            
-            final Collection<List<Integer>> list =   element.stream().collect(Collectors.groupingBy(e -> counter.getAndIncrement() / i))
-                            .values(); 
-
-            for(List<Integer> l : list){
-                finalRes.add(l);
-            }                
-        }
-
-
-
-        return finalRes;
+        return allGroups;
     }
 }
