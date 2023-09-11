8aecaadb7ee2c96955eeae2c085eab91d0e9e390
Ankit Pathak <78856709+ankitpathak530@users.noreply.github.com>
2023-09-11 14:00:45 +0530

    Time: 13 ms (7.81%), Space: 44.3 MB (70.67%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+          
+        List<Integer>[] res = new ArrayList[500];
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<500; i++) res[i] = new ArrayList<>();
+
+        List<List<Integer>> finalRes = new ArrayList<>();
+
+
+        for(int i=0; i<groupSizes.length; i++){
+            int idx = groupSizes[i]; //3
+            set.add(idx);
+            res[idx].add(i);
+        }
+
+
+        for(int i : set){ 
+            List<Integer> element = res[i];
+            // i -> 3    elemstSize- 6
+            AtomicInteger counter = new AtomicInteger();
+            final Collection<List<Integer>> list =   element.stream().collect(Collectors.groupingBy(l -> counter.getAndIncrement() / i))
+                            .values(); 
+
+            for(List<Integer> l : list){
+                finalRes.add(l);
+            }                
+        }
+
+
+
+        return finalRes;
+    }
+}
